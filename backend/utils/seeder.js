4cb0f8e2c5fe736d9a18dfd176b5ddec053c926c
Prahlad Singh <prahladsingh7512@gmail.com>
2026-06-30 22:32:27 +0530

    Update dotenv config path in seeder.js

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 const User = require('../models/User');
 const Story = require('../models/Story');
